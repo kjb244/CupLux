ea3088bcaebc3377d3d420be006fc6532a7cfc97
Kevin Bacino <kevin.bacino@gmail.com>
2016-09-23 18:32:08 -0400

    view js changed

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,7 +2,40 @@
 $(document).ready(function(){
 	$(document).foundation();
 
-	console.log('loaded');
+	var $target = $('.mainModule');
+
+	function fadeOutAll(clazz){
+		//if class is already show then don't do anything
+		if ( $target.find(clazz).hasClass('show') || clazz == undefined){
+			return;
+		}
+
+		$target.find('.main >.row.show').fadeOut(200, function(){
+			$target.find(clazz).fadeIn(1000).addClass('show');
+	
+				$(this).removeClass('show').addClass('hide');
+		
+		});
+	}
+
+	$target.find('a').on('click', function(){ 
+		
+		var destClass = $(this).attr('data-destination');
+
+		if (destClass){
+			destClass  = '.' + destClass;
+
+			fadeOutAll(destClass);
+
+		}
+		
+		
+	 });
+
+
+
+
+	
 
 
 });
