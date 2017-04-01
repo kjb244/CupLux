54aaf8a657e1cdeb295c04ce4af5d230bda113f9
kjb244 <kevin.bacino@gmail.com>
2017-04-01 09:36:52 -0400

    engagement links

--- build/javascripts/nature-for-all.js
+++ build/javascripts/nature-for-all.js
@@ -10,11 +10,23 @@ $(document).ready(function(){  var main = function(){
 		settings.$target.find('.menu-icon > a').on('click', function(){
 			if (Foundation.utils.is_small_only()){
 				setTimeout(function(){
-					console.log('trigger scroll');
 					$(window).scroll();
 				},100);
 			}
 		});
+
+		//activate current nav
+		var path = window.location.pathname.replace('/','');
+		var pathArr = path.split('-');
+
+		for(var i=0; i<pathArr.length; i++){
+			var $curr = settings.$target.find('a[id*="' + pathArr[i] + '"]');
+			if ($curr.length){
+				$curr.parent().addClass('current');
+				break;
+			}
+		}
+
 
 	}
 
