f89b7ee924c7945e7e9e16d3e03ee0214736ae75
kjb244 <kevin.bacino@gmail.com>
2017-04-11 18:03:33 -0400

    image interchange image changes

--- components/master/image-interchange-module/javascript/image-interchange-module.js
+++ components/master/image-interchange-module/javascript/image-interchange-module.js
@@ -1,5 +1,44 @@
 var main = function(){
+	var settings = {};
+	settings.$target = $('[data-module-image-interchange-module]')
 
-	$('.orbit-container').click();
+	getScreenSize = function(){
+		return (Foundation.utils.is_small_only() == true ? 'small': (Foundation.utils.is_medium_only() == true ? 'medium': 'large'));
+	}
+
+	storeImageSources = function(){
+		settings.$target.find('img').each(function(idx2, val2){
+				var imgSrc = $(val2).attr('src');
+				$(val2).attr('data-img-src', imgSrc);
+			});
+	}
+	voidMain = function(){
+		storeImageSources();
+
+		var screenSize = getScreenSize();
+		//hide and move image source so it doesn't download
+		$('.orbit-container ul').each(function(idx, val){
+			
+			var clazz = $(val).attr('class').toLowerCase();
+			
+			if (clazz.indexOf(screenSize) == -1){
+				
+				$(val).parent().hide();
+				$(val).find('img').attr('src', '');
+			}
+
+
+
+		});
+
+
+
+		$('.orbit-container').click();
+			
+
+	}
+
+	return voidMain();
+	
 
 }();
