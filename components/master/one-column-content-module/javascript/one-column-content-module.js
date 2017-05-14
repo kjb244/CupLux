f45b7ced001ecc395256d6c68519543dd7ee3cad
kjb244 <kevin.bacino@gmail.com>
2017-05-14 08:18:12 -0400

    final changes

--- components/master/one-column-content-module/javascript/one-column-content-module.js
+++ components/master/one-column-content-module/javascript/one-column-content-module.js
@@ -7,7 +7,7 @@ var main = function(){
 		var $faqLink = settings.$target.find('#faq-link, #read-more-link');
 		$faqLink.on('click', function(){
 			$(this).toggleClass('shown').next('img').toggleClass('rotateUp');
-			var $faqHidden = $(".data-module-rewards-faq, .data-module-content-nature-for-one");
+			var $faqHidden = $(".data-module-rewards-faq, .data-module-nature-for-one-bottom");
 			$faqHidden.slideToggle(1000);
 				if ($faqLink.hasClass('shown')){
 					var pixelsDown = $faqHidden.find('.row').first().offset().top;
