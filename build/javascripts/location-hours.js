1c205187ee5c2324e5fd88acb2bb2f2eb7ab1a9f
kjb244 <kevin.bacino@gmail.com>
2017-05-21 18:54:08 -0400

    final changes

--- build/javascripts/location-hours.js
+++ build/javascripts/location-hours.js
@@ -7,19 +7,6 @@ $(document).ready(function(){  var main = function(){
 	var voidMain = function(){
 
 
-		//activate current nav
-		var path = window.location.pathname.split('/').reverse()[0].replace('/','').replace('.html', '');
-		var pathArr = path.split('-');
-
-		for(var i=0; i<pathArr.length; i++){
-			var $curr = settings.$target.find('a[id*="' + pathArr[i] + '"]');
-			if ($curr.length){
-				$curr.parent().addClass('current');
-				break;
-			}
-		}
-
-
 	}
 
 	return voidMain();
