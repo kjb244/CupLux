a8fb6f7c3f740fdf0a09eecd00fa98c976a5ee2c
kjb244 <kevin.bacino@gmail.com>
2017-05-30 07:28:25 -0400

    post prod changes; added ga

--- Gruntfile.js
+++ Gruntfile.js
@@ -217,9 +217,19 @@ module.exports = function(grunt) {
 				let jqueryJS = "<script src='javascripts/jquery.js'></script>";
 				let foundationJS = "<script src='javascripts/foundation.min.js'></script>";
 				let jsDocumentReady2 = "<script src='javascripts/" + moduleObj['url'] + ".js'></script>";
+				let googleAnalytics = `<script>
+										  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+										  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+										  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+										  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+										  ga('create', 'UA-71578091-1', 'auto');
+										  ga('send', 'pageview');
+
+										</script>`;
 				let head = "<head>" + title + foundationMetaTag +  camptonBoldCss + camptonLightCss + 
 				           foundationCss + favicon + fontAwesomeCss + moduleCombinedCss + jqueryJS + foundationJS + 
-				           jsDocumentReady2 +  "</head>";
+				           jsDocumentReady2 + googleAnalytics + "</head>";
 
 
 				//finally write html file to file system
